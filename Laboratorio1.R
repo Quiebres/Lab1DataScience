aecc281d45257d19b1e6e45a916b6911833b4b95
Odalis <reyesgoe@gmail.com>
2019-08-14 13:26:13 -0600

    Resumen rápido
    Se cargó el archivo csv a R y se hizo una exploración rápida usando el comando de summary

---
+++ Laboratorio1.R
@@ -0,0 +1,5 @@
+# Leyendo el dataset de csv
+train <- read.csv('/Users/odalisrg/Documents/Data Science/Lab1DataScience/train.csv')
+
+# Exploración rápida del dataset usando un resumen
+summary(train)
