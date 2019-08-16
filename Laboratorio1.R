6cbb2cc9f6ead92862aab4c70e2bf1d71826846c
Unknown <iamaldonadocarrera@gmail.com>
2019-08-16 14:03:48 -0600

    Separación de Variables Cuantitativas y Cualitativas

--- Laboratorio1.R
+++ Laboratorio1.R
@@ -3,4 +3,8 @@ train <- read.csv("train.csv", TRUE, ",")
 # Volviendo el csv en un data frame
 class(train)
 # Exploración rápida del dataset usando un resumen
-summary(train)
+summary(train$HouseStyle)
+# Se separan las variables cuantitativas del dataset train
+trainCuan <- train[,c(4,5,18,19,20,21,27,35,37,38,39,44,45,46,47,48,49,50,51,52,53,55,57,60,62,63,67,68,69,70,71,72,76,77,78,81)]
+# Se separan las variables cualitativas del dataset train
+trainCual <- train[,-c(4,5,18,19,20,21,27,35,37,38,39,44,45,46,47,48,49,50,51,52,53,55,57,60,62,63,67,68,69,70,71,72,76,77,78,81)]
